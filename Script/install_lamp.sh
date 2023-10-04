a946090a93ae2915d48206c11388b629aef24f9b
Grah <antoniogranadosrubio@gmail.com>
2023-10-04 12:22:40 +0000

    añadir a script

--- Script/install_lamp.sh
+++ Script/install_lamp.sh
@@ -8,10 +8,12 @@ echo "Esto es una prueba"
 #apt upgrade -y
 
 #Instalar apache
-#apt install apache2 -y
+apt install apache2 -y
 
 #systemctl start apache2
 #systemctl stop apache2
 #systemctl restart apache2
 #systemctl reload apache2
 #systemctl status apache2
+
+sudo apt install mysql-server -y
