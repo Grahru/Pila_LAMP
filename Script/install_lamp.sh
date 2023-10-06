059f1987a8f6e4346d0e60cab63315d8936e7be2
Grah <antoniogranadosrubio@gmail.com>
2023-10-06 12:31:20 +0000

    Añadir .conf y  .php

--- Script/install_lamp.sh
+++ Script/install_lamp.sh
@@ -3,7 +3,7 @@ set -x
 #Muestra comandos
 echo "Esto es una prueba"
 #Actualizar repo
-#apt update
+apt update
 #Actualizar paquetes
 #apt upgrade -y
 
@@ -16,4 +16,17 @@ apt install apache2 -y
 #systemctl reload apache2
 #systemctl status apache2
 
+#Instalar MySQL
 sudo apt install mysql-server -y
+
+#Instalar php
+sudo apt install php libapache2-mod-php php-mysql -y
+
+#Copiar archivo de conf
+cp ../conf/000-default.conf /etc/apache2/sites-available
+
+systemctl restart apache2
+#Copiar index
+cp ../php/index.php /var/www/html
+#Modificar  propietario de /var/www/html al de apache
+chown -R www-data:www-data /var/www/html
